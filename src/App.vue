b2319edd2921fd03c9e42a661d517210c51bf608
yangzongwei <996959311@qq.com>
2021-02-03 17:48:20 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
   </div>
 </template>
 
